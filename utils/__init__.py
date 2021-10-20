5d5e36b37819484927f532a4d002121d22961c98
mbarbetti <matteo.barbetti.94@gmail.com>
2021-10-20 12:02:30 +0200

    added custom predictions

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@ from .plot_histos             import plot_histos
 from .DummyClassifier         import DummyClassifier
 from .display_score           import display_score
 from .precision_recall_scores import precision_recall_scores
+from .custom_predictions      import custom_predictions
