565246ff9ab36c6c40ab82585dc6f997083bc36b
mbarbetti <matteo.barbetti.94@gmail.com>
2021-10-20 16:47:40 +0200

    added auc curve

--- utils/custom_predictions.py
+++ utils/custom_predictions.py
@@ -10,7 +10,7 @@ def custom_predictions ( y_true : np.ndarray ,
                          precision_score : float = None ,
                          show_curves : bool = False ):
   if len(y_scores.shape) == 2:
-    y_scores = y_scores[:,0]
+    y_scores = y_scores[:,1]
 
   precision, recall, threshold = precision_recall_curve (y_true, y_scores)
   if show_curves:
@@ -25,17 +25,17 @@ def custom_predictions ( y_true : np.ndarray ,
     if (recall_score < 0) or (recall_score > 1):
       raise ValueError ("The recall score should be less than 1.")
     custom_threshold = threshold [np.argmin (recall >= recall_score) - 1]
-    return (y_scores >= custom_threshold)
+    return (y_scores >= custom_threshold), custom_threshold
 
   elif (precision is not None) and (recall_score is None):
     if (precision_score < 0) or (precision_score > 1):
       raise ValueError ("The precision score should be less than 1.")
     custom_threshold = threshold [np.argmax (precision >= precision_score)]
-    return (y_scores >= custom_threshold)
+    return (y_scores >= custom_threshold), custom_threshold
 
   elif (precision_score is None) and (recall_score is None):
     custom_threshold = threshold [np.argmin (recall > precision)]
-    return (y_scores >= custom_threshold)
+    return (y_scores >= custom_threshold), custom_threshold
 
   else:
     raise ValueError ("Only one target score should be passed: recall or precision.")
