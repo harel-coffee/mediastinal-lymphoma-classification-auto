108ac45cacabe01f78737ab052037833bfc71b68
mbarbetti <matteo.barbetti.94@gmail.com>
2021-10-21 16:14:57 +0200

    removed DummyClassifier

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from .data_cleaning           import data_cleaning
 from .plot_histos             import plot_histos
-from .DummyClassifier         import DummyClassifier
 from .display_score           import display_score
 from .precision_recall_scores import precision_recall_scores
 from .custom_predictions      import custom_predictions
+from .scatter_predictions     import scatter_predictions
